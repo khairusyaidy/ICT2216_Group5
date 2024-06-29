e6e883e2b00b611e2763ba5f8a08179b3c16a01c
zininngg <ziningtan12@gmail.com>
2024-06-29 17:40:43 +0800

    updated navbar

--- nav.inc.php
+++ nav.inc.php
@@ -36,18 +36,9 @@ if (isset($_POST['logout'])) {
         <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
             <div class="navbar-nav mr-auto py-0">
                 <a href="index.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : '' ?>">Home</a>
-                <a href="about.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'about.php' ? 'active' : '' ?>">About</a>
-                <a href="service.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'service.php' ? 'active' : '' ?>">Service</a>
-                <a href="price.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'price.php' ? 'active' : '' ?>">Price</a>
-                <a href="booking.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'booking.php' ? 'active' : '' ?>">Booking</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle <?= (basename($_SERVER['PHP_SELF']) == 'blog.php' || basename($_SERVER['PHP_SELF']) == 'single.php') ? 'active' : '' ?>" data-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu rounded-0 m-0">
-                        <a href="blog.php" class="dropdown-item <?= basename($_SERVER['PHP_SELF']) == 'blog.php' ? 'active' : '' ?>">Blog Grid</a>
-                        <a href="single.php" class="dropdown-item <?= basename($_SERVER['PHP_SELF']) == 'single.php' ? 'active' : '' ?>">Blog Detail</a>
-                    </div>
-                </div>
-                <a href="contact.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'contact.php' ? 'active' : '' ?>">Contact</a>
+                <a href="grooming.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'grooming.php' ? 'active' : '' ?>">Grooming</a>
+                <a href="daycare.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'daycare.php' ? 'active' : '' ?>">DayCare</a>
+                <a href="boarding.php" class="nav-item nav-link <?= basename($_SERVER['PHP_SELF']) == 'boarding.php' ? 'active' : '' ?>">Boarding</a>
             </div>
             <div class="navbar-nav ml-auto">
                 <?php if ($logged_in): ?>
