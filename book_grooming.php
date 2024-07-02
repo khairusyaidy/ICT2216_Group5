81fb7103e407872e325c95f8e689cdb977fd1c23
Ying Zhen <yingzhen0735@gmail.com>
2024-07-02 14:15:33 +0800

    Align the spacing for the 3 bookings

--- book_grooming.php
+++ book_grooming.php
@@ -98,6 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
         <div id="booking_successful_container">
+            <br>
             <h2>Please complete below form to book</h2>
 
             <form method="post" action="">
