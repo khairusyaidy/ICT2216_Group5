0652b75f2051fbd6aaf55b200402383e6a062c8e
zininngg <ziningtan12@gmail.com>
2024-07-02 14:16:11 +0800

    edit grooming page

--- grooming.php
+++ grooming.php
@@ -181,7 +181,7 @@
                     <button onclick="window.location.href='book_grooming.php'" style="padding: 10px 20px; font-size: 16px; background-color: #4CAF50; color: white; border: none; border-radius: 5px; cursor: pointer;">Book Now</button>
                 </div>
             </div>
-
+    </section>
             
             
             
